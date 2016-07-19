6ca4cd29feafd2da66fd58105fd209fbbecfa24b
JangHY <hyunyeong93@gmail.com>
2016-07-19 23:39:35 +0900

    수식 적용해서 주기적으로 값 구하기

--- Integral/app/src/main/java/com/example/samsung/orientationsensor/FileTest.java
+++ Integral/app/src/main/java/com/example/samsung/orientationsensor/FileTest.java
@@ -30,9 +30,12 @@ public class FileTest {
     PrintWriter outGravity;
     FileWriter writeKalman;
     PrintWriter outKalman;
+    FileWriter writeAverage;
+    PrintWriter outAverage;
     File fileOriginal;
     File fileGravity;
     File fileKalman;
+    File fileAverage;
     public FileTest(){
         /*SimpleDateFormat dateFormat = new  SimpleDateFormat("yyyyMMdd_HHMM", java.util.Locale.getDefault());
         Date date = new Date();
@@ -62,6 +65,13 @@ public class FileTest {
         }
         fileKalman = new File(path + fileName);
 
+        fileName = "sensor_average.csv";
+        fileAverage = new File(path + fileName);
+        if (fileAverage.exists()) {
+            fileAverage.delete();
+        }
+        fileAverage = new File(path + fileName);
+
         try{
             writeOriginal = new FileWriter(fileOriginal,true);
             outOriginal = new PrintWriter(writeOriginal);
@@ -80,6 +90,12 @@ public class FileTest {
             outKalman.print("kalmanData");
             outKalman.print(",");
             outKalman.close();
+
+            writeAverage = new FileWriter(fileAverage,true);
+            outAverage = new PrintWriter(writeAverage);
+            outAverage.print("averageData");
+            outAverage.print(",");
+            outAverage.close();
 
             Log.d("finish","저장완료");
         }
@@ -113,15 +129,41 @@ public class FileTest {
         catch(Exception e){
             e.printStackTrace();
         }
+    }
 
-/*
+    public void save(String averageData, String kalmanData){
         try{
-            fos = new FileOutputStream(file);
-            fos.write((strData).getBytes());
-            fos.close();
+            writeAverage = new FileWriter(fileAverage,true);
+            outAverage = new PrintWriter(writeAverage);
+            outAverage.print(averageData);
+            outAverage.print(",");
+            outAverage.close();
+
+            writeKalman = new FileWriter(fileKalman,true);
+            outKalman = new PrintWriter(writeKalman);
+            outKalman.print(kalmanData);
+            outKalman.print(",");
+            outKalman.close();
+
+            Log.d("finish","저장완료");
         }
-        catch (IOException e){
+        catch(Exception e){
             e.printStackTrace();
-        }*/
+        }
+    }
+
+    public void save(String originData){
+        try{
+            writeOriginal = new FileWriter(fileOriginal,true);
+            outOriginal = new PrintWriter(writeOriginal);
+            outOriginal.print(originData);
+            outOriginal.print(",");
+            outOriginal.close();
+
+            Log.d("finish","저장완료");
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
--- Integral/app/src/main/java/com/example/samsung/orientationsensor/MainActivity.java
+++ Integral/app/src/main/java/com/example/samsung/orientationsensor/MainActivity.java
@@ -7,10 +7,17 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Message;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.Collections;
 
 
 public class MainActivity extends Activity {
@@ -24,12 +31,19 @@ public class MainActivity extends Activity {
 
     float[] gravity = new float[3];
     float[] linear_acceleration = new float[3];
+    float[] average = new float[3];
     float[] kalmanFilter_acceleration = new float[3];
    // private float mX, mY;
     private KalmanFilter mKalmanAccX;
     private KalmanFilter mKalmanAccY;
     private KalmanFilter mKalmanAccZ;
 
+    float sumXYZAcc;//수식을 계산한 값
+
+    Customhandler1 customHandler1;
+    Customhandler2 customHandler2;
+
+    ArrayList[] accValue = new ArrayList[3];
 
     private final int MY_PERMISSION_REQUEST_STORAGE = 100;
 
@@ -55,13 +69,52 @@ public class MainActivity extends Activity {
         ky = (TextView)findViewById(R.id.kalmanFilter_y);
         kz = (TextView)findViewById(R.id.kalmanFilter_z);
         //mTextFileManager.save("짱이당");
-        checkPermission();
+
+        accValue[0] = new ArrayList();
+        accValue[1] = new ArrayList();
+        accValue[2] = new ArrayList();
+
+        Log.i("버전 정보 : ", String.valueOf(Build.VERSION.SDK_INT));
+        if(Build.VERSION.SDK_INT >= 23) {
+            checkPermission();
+        }
+
+
+
+        // checkPermission();//버전으로 나눠서 해줘야 됨
 
         //칼만필터 초기화
         mKalmanAccX = new KalmanFilter(0.0f);
         mKalmanAccY = new KalmanFilter(0.0f);
         mKalmanAccZ = new KalmanFilter(0.0f);
 
+        TimerTask timerTask1 = new TimerTask() {
+            @Override
+            public void run() {//0.001초 마다 센서 값받아서 소팅하고 리스트에 넣기
+                Message msg = customHandler1.obtainMessage();
+                customHandler1.sendMessage(msg);
+            }
+        };
+
+        Timer timer1 = new Timer();
+        timer1.schedule(timerTask1,0,2);//delay : 0, 주기 : 0.002초
+        //timer.schedule(timer, delayTime, period)
+        // delay:처음에 딜레이 되는 시간  period:주기(ms) 1초->1000   1->0.001초
+        customHandler1 = new Customhandler1();
+
+        TimerTask timerTask2 = new TimerTask() {
+            @Override
+            public void run() {//0.02초 마다 센서 필터링 해서 칼만필터에 넣기기
+               Message msg = customHandler2.obtainMessage();
+                customHandler2.sendMessage(msg);
+            }
+        };
+
+        Timer timer2 = new Timer();
+        timer2.schedule(timerTask2,18,20);//delay : 0, 주기 : 0.02초
+        //timer.schedule(timer, delayTime, period)
+        // delay:처음에 딜레이 되는 시간  period:주기(ms) 1초->1000   1->0.001초
+        customHandler2 = new Customhandler2();
 
     }
 
@@ -108,7 +161,8 @@ public class MainActivity extends Activity {
 
 
     private class accListener implements SensorEventListener {
-        public void onSensorChanged(SensorEvent event) {  // 가속도 센서 값이 "바뀔 때"마다 호출됨
+        public void onSensorChanged(SensorEvent event) {  // 가속도 센서 값이 "바뀔 때"마다 호출됨.
+           // 1초에 40번 정도 불러짐 -> 0.2초면 8번 정도 -> 가장 큰 값, 작은 값 뺴고 6개 평균내서 칼만필터 적용
            //https://developer.android.com/reference/android/hardware/SensorEvent.html#values
 
             // alpha is calculated as t / (t + dT)
@@ -116,6 +170,10 @@ public class MainActivity extends Activity {
             // and dT, the event delivery rate
 
             final float alpha = (float)0.8;
+
+            //original[0] = event.values[0];
+            //original[1] = event.values[1];
+            //original[2] = event.values[2];
 
             gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0]; //먼저 중력데이터를 계산함
             gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
@@ -126,9 +184,11 @@ public class MainActivity extends Activity {
             linear_acceleration[2] = event.values[2] - gravity[2];
 
             //칼만필터를 적용한다
-            kalmanFilter_acceleration[0] = (float) mKalmanAccX.update(linear_acceleration[0]);
-            kalmanFilter_acceleration[1] = (float) mKalmanAccY.update(linear_acceleration[1]);
-            kalmanFilter_acceleration[2] = (float) mKalmanAccZ.update(linear_acceleration[2]);
+//            kalmanFilter_acceleration[0] = (float) mKalmanAccX.update(linear_acceleration[0]);
+//            kalmanFilter_acceleration[1] = (float) mKalmanAccY.update(linear_acceleration[1]);
+//            kalmanFilter_acceleration[2] = (float) mKalmanAccZ.update(linear_acceleration[2]);
+
+
 
             ax.setText(Float.toString(event.values[0]));
             ay.setText(Float.toString(event.values[1]));
@@ -138,23 +198,11 @@ public class MainActivity extends Activity {
             gy.setText(Float.toString(linear_acceleration[1]));
             gz.setText(Float.toString(linear_acceleration[2]));
 
-            kx.setText(Float.toString(kalmanFilter_acceleration[0]));
-            ky.setText(Float.toString(kalmanFilter_acceleration[1]));
-            kz.setText(Float.toString(kalmanFilter_acceleration[2]));
+//            kx.setText(Float.toString(kalmanFilter_acceleration[0]));
+//            ky.setText(Float.toString(kalmanFilter_acceleration[1]));
+//            kz.setText(Float.toString(kalmanFilter_acceleration[2]));
 
-            mTextFileManager.save(Float.toString(event.values[0]), Float.toString(linear_acceleration[0]), Float.toString(kalmanFilter_acceleration[0]));
-
-            /*
-            * 부모 레이아웃을 스크롤시켜 마치 뷰객체(오브젝트)가 움직이는것처럼 보이게 한다
-              저장해둔 예전값과 현재값의 차를 넣어 변화를 감지한다
-              여기에 100을 곱하는것은 차의 숫자가 워낙 작아 움직임이 보이지 않기 때문이다.
-              즉, 스피드라고도 보면 된다ㅎㅎ 더 큰숫자를 넣으면 더 빠르게 움직인다.
-
-		      mLayout.scrollBy((int)((mX - filteredX) * 100), (int)((mY - filteredY) * 100));
-		      mX = filteredX;
-              mY = filteredY;
-            */
-
+//            mTextFileManager.save(Float.toString(event.values[0]), Float.toString(linear_acceleration[0]), Float.toString(kalmanFilter_acceleration[0]));
             Log.i("SENSOR", "Acceleration changed.");
             Log.i("SENSOR Original", "  Acceleration X: " + event.values[0]
                      + ", Acceleration Y: " + event.values[1]
@@ -164,4 +212,112 @@ public class MainActivity extends Activity {
         public void onAccuracyChanged(Sensor sensor, int accuracy) {
         }
     }
+
+
+    public class Customhandler1 extends android.os.Handler{
+        @Override
+        public void handleMessage(Message msg) {//0.001초마다 센서 값 받아와서 소팅하여 리스트에 넣기.
+            super.handleMessage(msg);
+            //이곳에 실행할 작업내용을 넣습니다. (메인 스레드 작업이 가능!)
+          //  Toast.makeText(getApplication(),"1초 마다"+accValue[0].size()+"  "+accValue[1].size()+"  "+accValue[2].size()+"  ",Toast.LENGTH_LONG).show();
+            Log.i("array value","0.001초 마다"+accValue[0].size()+"  "+accValue[1].size()+"  "+accValue[2].size());
+            //큰값, 작은값 빼고 평균 내기
+
+            accValue[0].add(linear_acceleration[0]);
+            accValue[1].add(linear_acceleration[1]);
+            accValue[2].add(linear_acceleration[2]);//리스트 값 삽입
+            String a = "";
+            for(int i = 0; i < accValue[0].size(); i++){
+                a = a +" "+ accValue[0].get(i);
+            }
+            Log.i("array list",a);
+            a = "";
+            Collections.sort(accValue[0]);
+            Collections.sort(accValue[1]);
+            Collections.sort(accValue[2]);//리스트 오름차순으로 소팅
+            for(int i = 0; i < accValue[0].size(); i++){
+                a = a +" "+ accValue[0].get(i);
+            }
+            Log.i("array list sort",a);
+        }
+    }
+
+    public class Customhandler2 extends android.os.Handler{
+        @Override
+        public void handleMessage(Message msg) {//0.01초마다 리스트에 있는 값들 필터링 해서 칼만필터로 보내기
+            super.handleMessage(msg);
+            //이곳에 실행할 작업내용을 넣습니다. (메인 스레드 작업이 가능!)
+            //  Toast.makeText(getApplication(),"1초 마다"+accValue[0].size()+"  "+accValue[1].size()+"  "+accValue[2].size()+"  ",Toast.LENGTH_LONG).show();
+            Log.i("array value","1초 마다"+accValue[0].size()+"  "+accValue[1].size()+"  "+accValue[2].size());
+            //큰값, 작은값 빼고 평균 내기
+
+            Log.i("array size 1",accValue[0].size()+"");//가장 작은 값 삭제
+            accValue[0].remove(0);
+            accValue[1].remove(0);
+            accValue[2].remove(0);
+            Log.i("array size 2",accValue[0].size()+"");
+
+            if(accValue[0].size() >= 1) {//가장 큰 값 삭제
+                accValue[0].remove(accValue[0].size() - 1);
+                accValue[1].remove(accValue[1].size() - 1);
+                accValue[2].remove(accValue[2].size() - 1);
+                Log.i("array size 3", accValue[0].size() + "");
+            }
+
+            float value0 = 0, value1 = 0, value2 = 0;
+            for(int i = 0; i < accValue[0].size(); i++){//리스트 값들 다 더하기
+                value0 = value0 + (float)accValue[0].get(i);
+                value1 = value1 + (float)accValue[1].get(i);
+                value2 = value2 + (float)accValue[2].get(i);
+            }
+
+            value0 = value0 / accValue[0].size();//평균 값 구하기
+            value1 = value0 / accValue[1].size();
+            value2 = value0 / accValue[2].size();
+
+            average[0] = value0;
+            average[1] = value1;
+            average[2] = value2;
+
+            Log.i("average",value0 +"  /  "+value1+"  /  "+value2);
+
+            kalmanFilter_acceleration[0] = (float) mKalmanAccX.update(value0);//칼만 필터에 적용
+            kalmanFilter_acceleration[1] = (float) mKalmanAccY.update(value1);
+            kalmanFilter_acceleration[2] = (float) mKalmanAccZ.update(value2);
+
+            sumXYZAcc = (float)Math.sqrt(kalmanFilter_acceleration[0]*kalmanFilter_acceleration[0] + kalmanFilter_acceleration[1]*kalmanFilter_acceleration[1] + kalmanFilter_acceleration[2]*kalmanFilter_acceleration[2]);
+           //수식 계산
+            kx.setText(Float.toString(kalmanFilter_acceleration[0]));
+            ky.setText(Float.toString(kalmanFilter_acceleration[1]));
+            kz.setText(Float.toString(kalmanFilter_acceleration[2]));
+
+            Log.i("textValue",Float.toString(average[0]) +"  /  "+Float.toString(linear_acceleration[0])+"  /  "+Float.toString(kalmanFilter_acceleration[0]));
+
+            mTextFileManager.save(Float.toString(average[0]) , Float.toString(kalmanFilter_acceleration[0]));
+
+
+
+
+            /***********************************************************************************
+             *
+             *
+             *
+             *
+             *
+             * 여기서 적분 함수를 부른다
+             * sumXYZAcc 가 더해진다.
+             *
+             *
+             *
+             *
+             *
+             * *******************************************************************************/
+
+
+            accValue[0].clear();
+            accValue[1].clear();
+            accValue[2].clear();
+        }
+    }
 }
+
